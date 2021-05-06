341429dedc380fa123ffd25843c73adcdfb242dc
Rafael de Souza Pereira <rafaelpereira.dev@gmail.com>
2021-05-05 21:46:57 -0300

    personalizado a tela de login

--- exercicio1-tela-login/app.js
+++ exercicio1-tela-login/app.js
@@ -1,4 +1,4 @@
-const btnLogin = document.querySelector('btn');
+const btnLogin = document.querySelector('.btn');
 const inputName = document.querySelector('.name');
 const inputPassword = document.querySelector('.password');
 const form = document.querySelector('.form');
@@ -11,21 +11,29 @@ document.addEventListener('click', (e) => {
   e.preventDefault();
 });
 
-
-
-
 window.addEventListener('click', (e) => {
   if (e.target.className == 'btn') {
     const textoName = inputName.value;
     const textoPassword = inputPassword.value;
-
     verificaLogin(textoName, textoPassword);
+    limpaInput();
   }
 });
+
+function limpaInput() {
+  if (inputName.value != '' && inputPassword.value != '') {
+    inputName.value = '';
+    inputPassword.value = '';
+  }
+}
 const usu = 'rafael.pereira';
 const senha = 'qwe@123';
 function verificaLogin(name, password) {
-  name !== usu && password !== senha
+  name !== usu || password !== senha
     ? (p.innerHTML = 'ERRO! Usuário ou Senha incorreto.')
     : (p.innerHTML = 'Login feito com sucesso');
 }
+
+setInterval(() => {
+  p.innerHTML = '';
+}, 5000);
