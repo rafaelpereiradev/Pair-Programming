4116ea5113f5f1ebb998d41de475fc3c9c75dcbe
wcocuzza <wallace.cocuzza@gmail.com>
2021-05-20 21:41:25 -0300

    Desenvolvido integração do Javascript com CSS para atualização das cores RGB

--- rgb-exercicio/app.js
+++ rgb-exercicio/app.js
@@ -1,39 +1,39 @@
 window.addEventListener('submit', (e) => {
   e.preventDefault();
 });
-const rangeRed = document.querySelectorAll('.rangered');
+
+const rangeRed = document.querySelector('.rangered');
 const rangeGreen = document.querySelector('.rangegreen');
 const rangeBlue = document.querySelector('.rangeblue');
 const inputRed = document.querySelector('.inputred');
 const inputGreen = document.querySelector('.inputgreen');
 const inputBlue = document.querySelector('.inputblue');
 const divRGB = document.querySelector('.rgb');
+divRGB.style.backgroundColor = 'rgb(0, 0, 0)'
 
 window.addEventListener('input', (e) => {
-  e.target.className == 'input rangered' ||
-  e.target.className == 'input inputred'
-    ? renderizaRed(e.target.value)
-    : e.target.className == 'input rangegreen' ||
-      e.target.className == 'input inputgreen'
-    ? renderizaGreen(e.target.value)
-    : e.target.className == 'input rangeblue' ||
-      e.target.className == 'input inputblue'
-    ? renderizaBlue(e.target.value)
-    : console.log('Faça sua cor');
+  e.target.className == 'input rangered' || e.target.className == 'input inputred'
+    ? renderizaRed(e.target.value) : e.target.className == 'input rangegreen' || e.target.className == 'input inputgreen'
+    ? renderizaGreen(e.target.value) : e.target.className == 'input rangeblue' || e.target.className == 'input inputblue'
+    ? renderizaBlue(e.target.value) : console.log('Faça sua cor');
 });
 
 function renderizaRed(value) {
-  inputRed.value = value && inputRed;
-  // inputRed.value = value;
-  console.log('red ok e');
+  rangeRed.value = value;
+  inputRed.value = value;
+  renderizaRGB();
 }
 function renderizaGreen(value) {
   rangeGreen.value = value;
   inputGreen.value = value;
-  console.log('green ok');
+  renderizaRGB();
 }
 function renderizaBlue(value) {
   rangeBlue.value = value;
   inputBlue.value = value;
-  console.log('blue ok');
+  renderizaRGB();
+}
+
+function renderizaRGB() {
+  divRGB.style.backgroundColor = `rgb(${rangeRed.value},${rangeGreen.value},${rangeBlue.value})`;
 }
