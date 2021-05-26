e61ae98ca8ac635ca01b379280a3e851cbf15a6b
Rafael de Souza Pereira <rafaelpereira.dev@gmail.com>
2021-05-25 21:55:33 -0300

    configurado server, e feito o link com o js

---
+++ exercicio-lista-tarefas/controllers/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const diretorioHtml = path.resolve('./views');
+
+//criando rota
+app.get('/', (req, res) => {
+  res.sendFile(path.join(diretorioHtml + '/index.html'));
+  console.log(diretorioHtml);
+});
+app.use(express.static(diretorioHtml));
+
+// Servidor é o ultimo a ser aberto.
+app.listen(3000);
---
+++ exercicio-lista-tarefas/views/main.js
@@ -0,0 +1,6 @@
+const inputLista = document.querySelector('#tarefa');
+const mostra = document.querySelector('#mostra');
+inputLista.textContent = 'teste';
+const p = document.createElement('p');
+p.textContent = 'ppppp';
+mostra.appendChild(p);
