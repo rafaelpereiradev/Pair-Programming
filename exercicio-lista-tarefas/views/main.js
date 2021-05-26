9b5cc2dd006ed21bb1ac6ca760ba60e63ff7e3ed
Rafael de Souza Pereira <rafaelpereira.dev@gmail.com>
2021-05-26 00:52:12 -0300

    configurado server, e feito o link com o js

--- exercicio-lista-tarefas/views/main.js
+++ exercicio-lista-tarefas/views/main.js
@@ -1,6 +1,10 @@
+window.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
+
 const inputLista = document.querySelector('#tarefa');
 const mostra = document.querySelector('#mostra');
 inputLista.textContent = 'teste';
 const p = document.createElement('p');
-p.textContent = 'ppppp';
+p.textContent = 'ppp';
 mostra.appendChild(p);
