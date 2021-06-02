31527605add5f559a1fbe7be533ba0b7b4280a52
Rafael de Souza Pereira <rafaelpereira.dev@gmail.com>
2021-06-01 23:09:33 -0300

    realizando refatoraçao no form de cadastro

--- exercicio-lista-tarefas/views/valida.js
+++ exercicio-lista-tarefas/views/valida.js
@@ -8,6 +8,9 @@ const inputUser = document.querySelector('.username-cadastro');
 const inputSenha = document.querySelector('.senha-cadastro');
 const btnCadastra = document.querySelector('#btn-cadastra');
 const divRenderiza = document.querySelector('#renderiza');
+const labelNome = document.querySelector('.label-nome');
+const labelUser = document.querySelector('.label-user');
+const labelSenha = document.querySelector('.label-senha');
 
 btnCadastra.addEventListener('click', (e) => {
   const nome = inputNome.value;
@@ -16,35 +19,41 @@ btnCadastra.addEventListener('click', (e) => {
   renderiza(nome, username, senha);
 });
 function renderiza(nome, user, senha) {
-  function criaP(titulo, valor) {
-    const verifica = (string) => /\s/g.test(string) || string.length == 0;
-    if (verifica(valor) == true) valor = 'caractere inválido, ou campo vazio.';
+  function criaP(titulo, valor, id) {
     const p = document.createElement('p');
-    p.innerHTML = `<b>${titulo}</b>${valor}`;
-    return p;
+    const verifica = (string) => /\s/g.test(string) || string.length == 0;
+    if (verifica(valor)) {
+      valor = 'caractere inválido, ou campo vazio.';
+      p.innerHTML = `<b>${titulo}</b>${valor}`;
+      p.classList.add(id);
+      console.log(p);
+      return trataErro(p);
+    } else {
+      p.innerHTML = `<b>${titulo}</b>${valor}`;
+      return p;
+    }
   }
-  const nomeRender = criaP('Nome do Usuário :', nome);
-  const userRender = criaP('Username : ', user);
-  const senhaRender = criaP('Senha : ', senha);
 
-  divRenderiza.appendChild(nomeRender);
-  divRenderiza.appendChild(userRender);
-  divRenderiza.appendChild(senhaRender);
-  criaBtnEdita();
-  criaBtnOk();
-  // divRenderiza.appendChild(btnEdita);
+  const dadosUsuario = {
+    nome: criaP('Nome do Usuário :', nome, 'nome'),
+    username: criaP('Username : ', user, 'user'),
+    senha: criaP('Senha : ', senha, 'senha'),
+  };
+
+  const quebra = document.createElement('br');
+
+  divRenderiza.appendChild(dadosUsuario.nome);
+  divRenderiza.appendChild(dadosUsuario.username);
+  divRenderiza.appendChild(dadosUsuario.senha);
+  divRenderiza.appendChild(quebra);
 }
 
-function criaBtnOk() {
-  const btnRegistra = document.createElement('button');
-  btnRegistra.classList.add('btn-login');
-  btnRegistra.textContent = 'Cadastrar';
-  divRenderiza.appendChild(btnRegistra);
-}
-function criaBtnEdita() {
-  const btnEdita = document.createElement('button');
-  btnEdita.style.marginLeft = '40px';
-  btnEdita.classList.add('btn-login');
-  btnEdita.textContent = 'Editar';
-  divRenderiza.appendChild(btnEdita);
+function trataErro(p) {
+  p.className == 'nome'
+    ? (p.style.color = `rgb(255,0,0)`)(labelNome.appendChild(p))
+    : p.className == 'user'
+    ? (p.style.color = `rgb(255,0,0)`)(labelUser.appendChild(p))
+    : p.className == 'senha'
+    ? (p.style.color = `rgb(255,0,0)`)(labelSenha.appendChild(p))
+    : mostraDados();
 }
