e9864a50f47e2f575fe43beed73a104378fa4638
Rafael de Souza Pereira <rafaelpereira.dev@gmail.com>
2021-06-02 19:43:45 -0300

    Finalizado Front-End cadastro

--- exercicio-lista-tarefas/src/valida.js
+++ exercicio-lista-tarefas/src/valida.js
@@ -11,49 +11,189 @@ const divRenderiza = document.querySelector('#renderiza');
 const labelNome = document.querySelector('.label-nome');
 const labelUser = document.querySelector('.label-user');
 const labelSenha = document.querySelector('.label-senha');
+const span = document.getElementById('span');
 
-btnCadastra.addEventListener('click', (e) => {
-  const nome = inputNome.value;
-  const username = inputUser.value;
-  const senha = inputSenha.value;
-  renderiza(nome, username, senha);
-});
-function renderiza(nome, user, senha) {
-  function criaP(titulo, valor, id) {
+btnCadastra.addEventListener('click', () => {
+  const nome = verifica(inputNome.value);
+  const username = verifica(inputUser.value);
+  const senha = verifica(inputSenha.value);
+
+  if (nome) {
+    erroNome(nome);
+  }
+  if (username) {
+    erroUser(username);
+  }
+  if (senha) {
+    erroSenha(senha);
+  }
+  if (nome == false && username == false && senha == false) {
+    renderizaOk();
+  }
+  console.log(nome, username, senha);
+  function verifica(valor) {
+    return /\s/g.test(valor) || valor.length == 0;
+  }
+
+  function criaP() {
     const p = document.createElement('p');
-    const verifica = (string) => /\s/g.test(string) || string.length == 0;
-    if (verifica(valor)) {
-      valor = 'caractere inválido, ou campo vazio.';
-      p.innerHTML = `<b>${titulo}</b>${valor}`;
-      p.classList.add(id);
-      console.log(p);
-      return trataErro(p);
-    } else {
-      p.innerHTML = `<b>${titulo}</b>${valor}`;
-      return p;
+    return p;
+  }
+
+  function erroNome() {
+    const p = criaP();
+    p.innerHTML = 'contêm caractére inválido,ou está vazio.';
+    if (labelNome.childElementCount == 2) {
+      return;
+    }
+    labelNome.appendChild(p);
+    p.style.color = `rgb(255,0,0)`;
+    return;
+  }
+  function erroUser() {
+    const p = criaP();
+    p.innerHTML = 'contêm caractére inválido,ou está vazio.';
+    if (labelUser.childElementCount == 2) {
+      return;
+    }
+    labelUser.appendChild(p);
+    p.style.color = `rgb(255,0,0)`;
+    return;
+  }
+  function erroSenha() {
+    const p = criaP();
+    p.innerHTML = 'contêm caractére inválido,ou está vazio.';
+    if (labelSenha.childElementCount == 2) {
+      return;
+    }
+    labelSenha.appendChild(p);
+    p.style.color = `rgb(255,0,0)`;
+    return;
+  }
+  function renderizaOk() {
+    const nomeOk = criaP();
+    const userOk = criaP();
+    const senhaOk = criaP();
+    const titulo = document.createElement('h3');
+    render();
+    function render() {
+      nomeOk.innerHTML = `<b>Nome</b> : ${inputNome.value}`;
+      userOk.innerHTML = `<b>Username</b> : ${inputUser.value}`;
+      senhaOk.innerHTML = `<b>Senha</b> : ${inputSenha.value}`;
+      titulo.innerText = 'Usuário criado com sucesso!';
+
+      divRenderiza.appendChild(titulo);
+      divRenderiza.appendChild(nomeOk);
+      divRenderiza.appendChild(userOk);
+      divRenderiza.appendChild(senhaOk);
     }
   }
 
-  const dadosUsuario = {
-    nome: criaP('Nome do Usuário :', nome, 'nome'),
-    username: criaP('Username : ', user, 'user'),
-    senha: criaP('Senha : ', senha, 'senha'),
-  };
+  // function renderizaOkUser() {
+  //   const p = criaP();
+  //   p.innerHTML = inputUser.value;
+  //   divRenderiza.appendChild(p);
+  //   return;
+  // }
+  // function renedrizaOkSenha() {
+  //   const p = criaP();
+  //   p.innerHTML = inputUser.value;
+  //   divRenderiza.appendChild(p);
+  //   return;
+  // }
+});
 
-  const quebra = document.createElement('br');
+inputNome.addEventListener('click', (e) => {
+  if (labelNome.childElementCount == 2) {
+    labelNome.lastElementChild.remove();
+  }
+});
+inputUser.addEventListener('click', (e) => {
+  if (labelUser.childElementCount == 2) {
+    labelUser.lastElementChild.remove();
+  }
+});
+inputSenha.addEventListener('click', (e) => {
+  if (labelSenha.childElementCount == 2) {
+    labelSenha.lastElementChild.remove();
+  }
+});
 
-  divRenderiza.appendChild(dadosUsuario.nome);
-  divRenderiza.appendChild(dadosUsuario.username);
-  divRenderiza.appendChild(dadosUsuario.senha);
-  divRenderiza.appendChild(quebra);
-}
+//renderiza(nomeVerificado, usernameVerificado, senhaVerificada);
 
-function trataErro(p) {
-  p.className == 'nome'
-    ? (p.style.color = `rgb(255,0,0)`)(labelNome.appendChild(p))
-    : p.className == 'user'
-    ? (p.style.color = `rgb(255,0,0)`)(labelUser.appendChild(p))
-    : p.className == 'senha'
-    ? (p.style.color = `rgb(255,0,0)`)(labelSenha.appendChild(p))
-    : mostraDados();
-}
+// function renderiza(nome, user, senha) {
+//   const dadosUsuario = {
+//     nome: criaP('Nome do Usuário :', nome, 'nome'),
+//     username: criaP('Username : ', user, 'user'),
+//     senha: criaP('Senha : ', senha, 'senha'),
+//   };
+
+// function criaP(titulo, valor, id) {
+//   const p = document.createElement('p');
+//   if (verifica(valor)) {
+//     p.innerHTML = `${titulo} contêm caractére inválido,ou está vazio.`;
+//     p.classList.add(id);
+
+//     p.classList.add('erro');
+//     return trataErro(p);
+//   }
+//   p.innerHTML = `<b>${titulo}</b>${valor}`;
+//   p.classList.add(id);
+//   return p;
+// }
+// criaForm(dadosUsuario);
+// }
+
+// function criaForm(obj) {
+//   const quebra = document.createElement('br');
+//   if (
+//     obj.nome != undefined &&
+//     obj.username != undefined &&
+//     obj.senha != undefined
+//   ) {
+//     divRenderiza.appendChild(obj.nome);
+//     divRenderiza.appendChild(obj.username);
+//     divRenderiza.appendChild(obj.senha);
+//     divRenderiza.appendChild(quebra);
+//   }
+//   // trataErro();
+// }
+// function trataErro(p) {
+//   if (p.className == 'erro') {
+//     labelNome.appendChild(p);
+//   }
+// if (typeof p == undefined) {
+//   labelNome.appendChild(p);
+//   console.log(p);
+// }
+// }
+// if (p.className == 'user erro' && labelUser.childElementCount > 1) {
+//   labelUser.appendChild(p);
+// }
+// if (p.className == 'senha erro' && labelSenha.childElementCount > 1) {
+//   labelSenha.appendChild(p);
+// }
+// limpaEssa();
+// }
+
+// function limpaEssa() {
+//   if (labelNome.childElementCount > 1) {
+//     labelNome.lastChild.remove();
+//   }
+// }
+
+//     }
+//   });
+
+//   if (e.target.id == '#btn-cadastra') {
+//     user.childNodes.forEach((element) => {
+//       element.className == 'user erro'.remove();
+//     });
+//   }
+// }
+
+// if (e.target.id == '#btn-cadastra') {
+//   senha.childNodes.forEach((element) => {
+//     element.className == 'senha erro'.remove();
+//   });
+// }
