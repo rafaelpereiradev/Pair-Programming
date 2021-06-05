be65bd8ebf35023be1ae165a9f18be5b053f1d8d
Rafael de Souza Pereira <rafaelpereira.dev@gmail.com>
2021-06-05 01:58:36 -0300

    refatorado para melhoria de alerta dos inputs

--- exercicio-lista-tarefas/public/src/cadastro.js
+++ exercicio-lista-tarefas/public/src/cadastro.js
@@ -14,35 +14,55 @@ const labelSenha = document.querySelector('.label-senha');
 const span = document.getElementById('span');
 
 btnCadastra.addEventListener('click', function iniciaForm() {
+  //verificando se já existe uma renderização para o usuário.
+  if (divRenderiza.childElementCount == 4) {
+    btnCadastra.removeEventListener('click', iniciaForm);
+    return;
+  }
   const nome = verificaNome(inputNome.value);
   const username = verifica(inputUser.value);
   const senha = verifica(inputSenha.value);
 
+  //envio dos inputs para tratamento
   function verificaNome(nome) {
     if (nome <= 0) {
+      // focus();
       erroNome();
       return true;
     }
   }
   if (username) {
     erroUser();
+    focus();
   }
   if (senha) {
+    focus();
     erroSenha();
   }
+
+  function focus() {
+    nome
+      ? inputNome.focus()
+      : username
+      ? inputUser.focus()
+      : senha
+      ? inputSenha.focus()
+      : null;
+  }
+
   if (!nome && !username && !senha) {
     renderizaOk();
   }
-
+  // regex, verifica se existe algum espaço em branco somente para username e senha.
   function verifica(valor) {
     return /\s/g.test(valor) || valor.length == 0;
   }
-
+  //função para criar elementos P
   function criaP() {
     const p = document.createElement('p');
     return p;
   }
-
+  //criando os alertas nos inputs.
   function erroNome() {
     const p = criaP();
     p.innerHTML = 'contêm caractére inválido,ou está vazio.';
@@ -73,21 +93,21 @@ btnCadastra.addEventListener('click', function iniciaForm() {
     p.style.color = `rgb(255,0,0)`;
     return;
   }
+  //criando os elementos para renderizar
   function renderizaOk() {
     const nomeOk = criaP();
     const userOk = criaP();
     const senhaOk = criaP();
     const titulo = document.createElement('h3');
     render();
+
+    // Renderizando as Informações do Usuário
     function render() {
       nomeOk.innerHTML = `<b>Nome</b> : ${inputNome.value.trim()}`;
       userOk.innerHTML = `<b>Username</b> : ${inputUser.value}`;
       senhaOk.innerHTML = `<b>Senha</b> : ${inputSenha.value}`;
       titulo.innerText = 'Usuário criado com sucesso!';
-      if (divRenderiza.childElementCount == 4) {
-        btnCadastra.removeEventListener('click', iniciaForm);
-        return;
-      }
+
       divRenderiza.appendChild(titulo);
       divRenderiza.appendChild(nomeOk);
       divRenderiza.appendChild(userOk);
@@ -95,19 +115,24 @@ btnCadastra.addEventListener('click', function iniciaForm() {
     }
   }
 });
-
-inputNome.addEventListener('click', (e) => {
-  if (labelNome.childElementCount == 2) {
+//limpando os alertas dos inputs
+window.addEventListener('keyup', (e) => {
+  if (
+    e.target.className == 'nome-cadastro' &&
+    labelNome.childElementCount == 2
+  ) {
     labelNome.lastElementChild.remove();
   }
-});
-inputUser.addEventListener('click', (e) => {
-  if (labelUser.childElementCount == 2) {
+  if (
+    e.target.className == 'username-cadastro' &&
+    labelUser.childElementCount == 2
+  ) {
     labelUser.lastElementChild.remove();
   }
-});
-inputSenha.addEventListener('click', (e) => {
-  if (labelSenha.childElementCount == 2) {
+  if (
+    e.target.className == 'senha-cadastro' &&
+    labelSenha.childElementCount == 2
+  ) {
     labelSenha.lastElementChild.remove();
   }
 });
